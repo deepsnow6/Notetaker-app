275a7cee2fa638496b31c61d02e5295ee095f902
deepsnow6 <denirensign@aol.com>
2023-07-05 18:39:43 -0600

    content commit

---
+++ Develop/server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// HTML Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'));
+});
+
+// API Routes
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error reading notes.' });
+    } else {
+      const notes = JSON.parse(data);
+      res.json(notes);
+    }
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = generateUniqueId();
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error saving note.' });
+    } else {
+      const notes = JSON.parse(data);
+      notes.push(newNote);
+
+      fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Error saving note.' });
+        } else {
+          res.json(newNote);
+        }
+      });
+    }
+  });
+});
+
+// Helper function to generate a unique ID for each note
+function generateUniqueId() {
+  return uuidv4();
+}
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
